dca81fb09a7246af712fc743de823cdc802a5eb6
GOPAL-GUPTA <45972364+GOPAL-GUPTA@users.noreply.github.com>
2023-07-06 06:50:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from flask import Flask,request,abort,jsonify
+from flask import Flask
 
 app =Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'hello world for azure'
-@app.route('/first')
-def first():
-    if(request.method == 'GET'):
-        data = {
-            "Modules" : 15,
-            "Subject" : "Data Structures and Algorithms",
-        }
-        return jsonify(data)
+# @app.route('/first')
+# def first():
+#     # if(request.method == 'GET'):
+#     #     data = {
+#     #         "Modules" : 15,
+#     #         "Subject" : "Data Structures and Algorithms",
+#     #     }
+#     #     return jsonify(data)
     
-@app.route('/webhook',methods =['GET','POST'])
-def webhook():
-    if request.method=='POST':
-        print(request.json)
-        data = request.json
-        return 'success'
+# @app.route('/webhook')
+# def webhook():
+#     # if request.method=='POST':
+#     #     print(request.json)
+#     #     data = request.json
+#     #     return 'success'
         
 if __name__=='__main__':
     app.run()
